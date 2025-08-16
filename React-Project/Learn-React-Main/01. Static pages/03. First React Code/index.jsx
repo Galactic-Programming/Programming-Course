49ace3c079fe3fb09fc00d995cb6702ae13ecee9
Yuri Volkov <yurivolkov1210@gmail.com>
2025-08-16 11:47:47 +0700

    Upload #01 Static-pages from "Scrimba" Add initial setup for Section 1 Recap project
    - Created index.html with basic structure and linked CSS and JS files
    - Added index.css for styling the application
    - Implemented index.jsx to render the main App component using React
    - Configured package.json with necessary dependencies and scripts for Vite
    - Set up vite.config.js to use the React plugin for Vite

---
+++ React-Project/Learn-React-Main/01. Static pages/03. First React Code/index.jsx
@@ -0,0 +1,9 @@
+// Challenge: Re-write the first lines of React code we just had.
+// You can render anything you want to the screen, doesn't have
+// to be an h1 element.
+
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')).render(
+    <h1>React is a JavaScript library for building user interfaces</h1>
+);
